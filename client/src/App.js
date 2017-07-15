8c089bedd808fd42202a72e07aecf43ba6d9113b
Thomas Nagengast <tnagengast@gmail.com>
2017-07-15 02:19:24 -0700

    Update branding to Riptide Notes

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
                 <Navbar fluid collapseOnSelect>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <Link to="/">Scratch</Link>
+                            <Link to="/">Riptide Notes</Link>
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { PageHeader, ListGroup, ListGroupItem, } from 'react-bootstrap';
 import { invokeApig } from '../aws';
 import './Home.css';
@@ -65,8 +65,12 @@ class Home extends Component {
     renderLander() {
         return (
             <div className="lander">
-                <h1>Riptide</h1>
+                <h1>Riptide Notes</h1>
                 <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">Login</Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">Signup</Link>
+                </div>
             </div>
         );
     }
